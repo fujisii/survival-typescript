c5b5867e31e254607979b947a6ccbbecdfecdd94
fujisii <51264835+fujisii@users.noreply.github.com>
2022-10-05 15:56:22 +0900

    update random-cat - APIのレスポンスに型付け

--- random-cat/src/pages/index.tsx
+++ random-cat/src/pages/index.tsx
@@ -11,14 +11,30 @@ const randomCatImage = (): string => {
   return catImages[index];
 };
 
-const fetchCatImage = async () => {
+interface CatCategory {
+  id: number;
+  name: string;
+}
+
+interface SearchCatImage {
+  breeds: string[];
+  categories: CatCategory[];
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+}
+
+type SearchCatImageResponse = SearchCatImage[];
+
+const fetchCatImage = async (): Promise<SearchCatImage> => {
   const res = await fetch("https://api.thecatapi.com/v1/images/search");
-  const result = await res.json();
+  const result = (await res.json()) as SearchCatImageResponse;
   return result[0];
 };
 
 fetchCatImage().then((image) => {
-  console.log(`猫の画像: ${image.url}`);
+  console.log(image.alt);
 });
 
 const IndexPage = () => {
